8517af2596e3cce009ca6c4fe53479d2588e0e6c
MukilMuthu <72900502+MukilMuthu@users.noreply.github.com>
2021-02-15 18:50:40 +0530

    added demo movement
    keyboard and joypad toggle button and movement prototype

---
+++ Assets/Scripts/Joystick.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour,IDragHandler,IPointerUpHandler,IPointerDownHandler
+{
+    private Image joyBG;
+    private Image joyPad;
+    private Vector3 inputVector;
+
+    public Vector3 JoyPadInputVector => new Vector3(inputVector.x, 0, inputVector.z);
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(joyBG.rectTransform, eventData.position, eventData.pressEventCamera, out pos))
+        {
+            pos.x = (pos.x / joyBG.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / joyBG.rectTransform.sizeDelta.y);
+
+            inputVector = new Vector3(pos.x * 2 + 1, 0, pos.y * 2 - 1);
+            inputVector = (inputVector.magnitude > 1.0f ? inputVector.normalized : inputVector);
+
+            joyPad.rectTransform.anchoredPosition = new Vector3(inputVector.x * (joyBG.rectTransform.sizeDelta.x / 3), inputVector.z * (joyBG.rectTransform.sizeDelta.y / 3),0f);
+        }
+     
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        inputVector = Vector3.zero;
+        joyPad.rectTransform.anchoredPosition = Vector3.zero;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joyBG = GetComponent<Image>();
+        joyPad = transform.GetChild(0).GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        Debug.Log(JoyPadInputVector);
+    }
+
+}
---
+++ Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum InputType { Keyboard, JoyStick}
+public class PlayerActions : MonoBehaviour
+{
+    public Button ToggleBTN;
+    public Image HpFill;
+    public float Speed;
+    public float RotationSpeed;
+    public InputType inputMode;
+    public Joystick Joystick;
+
+    private Vector3 movementVector;
+    private Vector3 movementDirection;
+
+
+
+    private float smoothVelocity;
+    private float horizontal;
+    private float vertical;
+
+    private bool isSafe = false;
+
+    private void Awake()
+    {
+        ToggleBTN.onClick.AddListener(ToggleInput);
+        inputMode = InputType.Keyboard;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Finish")
+        {
+            isSafe = true;
+            Debug.Log("HIT TRIGGER!");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Finish")
+        {
+            isSafe = false;
+            Debug.Log("EXITED SAFE ZONE!");
+        }
+    }
+    private void ToggleInput()
+    {
+        if (inputMode == InputType.Keyboard)
+        {
+            inputMode = InputType.JoyStick;
+            ToggleBTN.GetComponentInChildren<Text>().text = "JoyStick!";
+        }
+        else if(inputMode == InputType.JoyStick)
+        {
+            inputMode = InputType.Keyboard;
+            ToggleBTN.GetComponentInChildren<Text>().text = "Keyboard!";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isSafe)
+        {
+            HpFill.fillAmount -= Time.deltaTime * 0.1f;
+        }
+        if(inputMode == InputType.Keyboard)
+        {
+            horizontal = Input.GetAxisRaw("Horizontal");
+            vertical = Input.GetAxisRaw("Vertical");
+
+        }
+        else
+        {
+            horizontal = Joystick.JoyPadInputVector.x;
+            vertical = Joystick.JoyPadInputVector.z;
+        }
+            movementVector = new Vector3(horizontal, 0, vertical) * Time.deltaTime * Speed;
+            movementDirection = movementVector.normalized;
+
+        float targetAngle = Mathf.Atan2(movementDirection.x, movementDirection.z) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothVelocity, RotationSpeed);
+
+        if (movementDirection.magnitude > 0f)
+        {
+             this.transform.position += movementVector;
+            this.transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+        }
+        
+    }
+}
