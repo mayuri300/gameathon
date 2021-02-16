84281f70fa4e02bcb6f37b5b2e54ca9275475850
MukilMuthu <72900502+MukilMuthu@users.noreply.github.com>
2021-02-16 10:55:17 +0530

    Camera Follow
    Implemented Camera Follow Logic and Main Menu sample

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    public Vector3 Offset;
+    public float SmoothValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredPos = Target.position + Offset;
+        Vector3 smoothPos = Vector3.Lerp(desiredPos, this.transform.position, SmoothValue * Time.deltaTime);
+        this.transform.position = smoothPos;
+    }
+}
