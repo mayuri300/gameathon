dfe06024e64739c7c5c8720f54e71a27b52b57a8
MukilMuthu <72900502+MukilMuthu@users.noreply.github.com>
2021-02-16 14:43:57 +0530

    Async Load
    Prototype Asynchronous loading with game tips.

---
+++ Assets/Scripts/MainMenuNavigation.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuNavigation : MonoBehaviour
+{
+    public Button PlayGameBTN;
+    public Button ExitGameBTN;
+    public Image LoadingPanel;
+    public Text LoadingProgressText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayGameBTN.onClick.AddListener(PlayGame);
+        ExitGameBTN.onClick.AddListener(ExitGame);
+    }
+    private void OnDestroy()
+    {
+        PlayGameBTN.onClick.RemoveAllListeners();
+        ExitGameBTN.onClick.RemoveAllListeners();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        //Async Load into Game Scene
+        StartCoroutine(LoadGame());
+    }
+    IEnumerator LoadGame()
+    {
+        yield return null;
+        AsyncOperation ao = SceneManager.LoadSceneAsync(1);
+        ao.allowSceneActivation = false;
+        while (!ao.isDone)
+        {
+            //display loading text and progress
+            LoadingPanel.gameObject.SetActive(true);
+            LoadingProgressText.text = "Loading : " + (ao.progress * 100) + "%";
+            yield return new WaitForSeconds(2f); //remove later
+            if(ao.progress >= 0.97f)
+            {
+                ao.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
